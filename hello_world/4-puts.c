6631643b413b9e1fad32ea388160b5ee6b98c071
wahle1997 <Erica.Wahl@atlasschool.com>
2024-01-18 23:39:22 +0000

    c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#!/bin/bash
-# Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+#Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
 #Use the function puts
 
 #include <stdio.h>
-
 int main(void)
 {
-  puts("\"Promming is like building a multilingual puzzle");
-
-  return (0);
+        puts("\"Promming is like building a multilingual puzzle");
+        return (0);
 }
