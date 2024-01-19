e112419b842127132ef6aa0c21e6585d9a8733d4
wahle1997 <Erica.Wahl@atlasschool.com>
2024-01-19 00:03:35 +0000

    c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
-* 
+ *main - Entry point of the program
+ * 
  * Description: Using put function
  *
-* Return: Always 0 (success) 
+ *Return: Always 0 (success) 
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
