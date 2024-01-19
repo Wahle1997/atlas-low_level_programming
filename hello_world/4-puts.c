d4e62f796c1e75516ce28d1bf586098105537bc0
wahle1997 <Erica.Wahl@atlasschool.com>
2024-01-19 00:08:32 +0000

    c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  *main - Entry point of the program
  * 
- * Description: Using put function
- * Return: Always 0 (success)
+ *Description: Using put function
+ *Return: Always 0 (success)
  * 
  */
 int main(void)
