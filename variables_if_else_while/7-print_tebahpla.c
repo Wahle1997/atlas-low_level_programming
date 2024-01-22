76049d642df3d19b91a7cfca9c6b7096078196c9
wahle1997 <Erica.Wahl@atlasschool.com>
2024-01-22 00:01:52 +0000

    ifelse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
- /**
-  * main - Prints the alphabet in lowercase in reverse.
-  *
-  * Return: Always 0 (Success)
-  */
- int main(void)
- {
-   char letter;
 
-   for (letter = 'z'; letter >= 'a'; --letter)
-     {
-       putchar(letter);
-     }
-   putchar('\n');
-   return (0);
- }~
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
