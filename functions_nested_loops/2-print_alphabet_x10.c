2d2b4eaf3dc9a17c2b33bbc723f53c7c4bd0be00
wahle1997 <Erica.Wahl@atlasschool.com>
2024-01-23 01:52:22 +0000

    nestedloops

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,18 @@
 #include "main.h"
-
 /**
-  * print_alphabet_x10 - Make alphabet x10 times
-  *
-  * Return: void
-  */
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times followed
+ *                      by a new line.
+ */
 void print_alphabet_x10(void)
 {
-        char letter;
+	char letter;
 	int i;
-
 	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
-
 		_putchar('\n');
-
 	}
 }
