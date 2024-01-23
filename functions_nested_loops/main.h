783d5d14008133b5411a214d8ad1811aae680656
wahle1997 <Erica.Wahl@atlasschool.com>
2024-01-23 02:23:58 +0000

    nestedloops

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);
 
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,4 +16,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-#endif MAIN
+#endif */ MAIN */
